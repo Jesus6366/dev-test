b8747e919097b766f5f40ac26096d92fac622668
unknown <polibos6366@gmail.com>
2024-11-23 14:46:18 -0700

    react-router-dom package install and routes created for login and user profile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="user" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
